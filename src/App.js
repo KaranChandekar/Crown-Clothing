372be04c6301c2aa5aa9ba178070491990c9741f
Karan Chandekar <karanchandekar431@gmail.com>
2022-08-06 19:46:24 +0530

    building shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
